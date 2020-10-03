91d6e62eb42f72e6506ccda77660018bf85f243b
Alex K (via Travis CI) <alexkurasa@gmail.com>
2020-10-03 12:28:54 +0000

    Deploy garevna/nails to github.com/garevna/nails.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nails/precache-manifest.e7a3c354d03f90c1822d8f20b0daa6e3.js"
+  "/nails/precache-manifest.75ddc18b8a188d1d39e2652205f398d6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "nails"});
