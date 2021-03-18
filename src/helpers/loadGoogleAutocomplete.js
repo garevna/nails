4f7629372da7cdd885ae3ba5f1d79d7cce837472
sergei <tustanovskiis@gmail.com>
2021-03-18 15:23:45 +0200

    add autocomplete

--- src/helpers/loadGoogleAutocomplete.js
+++ src/helpers/loadGoogleAutocomplete.js
@@ -1,9 +1,5 @@
 export default function () {
   return new Promise(resolve => {
-    // if (window?.google?.maps) {
-    //   resolve.bind(null, true);
-    //   return
-    // }
     const script = document.body.appendChild(document.createElement('script'));
     script.src = `${process.env.VUE_APP_API_URL_GOOGLE}/maps/api/js?key=${process.env.VUE_APP_GOOGLE_MAPS_KEY}&libraries=places`;
     script.onload = resolve.bind(null, true);
