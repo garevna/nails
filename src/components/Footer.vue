f1bff1b2fcf1bc13f70cf0f448250d92f88eae24
Sergey Tustanovskii <tustanovskiis@gmail.com>
2021-06-06 14:40:14 +0300

    fix fonts in the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,68 +1,67 @@
 <template>
   <v-footer class="homefone pa-0 pb-6">
-    <v-card flat tile class="orange" width="100%">
-      <v-container>
-        <v-row>
-          <v-col cols="12" sm="6" md="3" order="4" order-md="1">
-            <v-card flat class="transparent d-flex justify-center justify-sm-start pt-4">
-              <v-btn icon href="https://www.instagram.com/nailsaustralia" target="_blank" fab>
-                <IconInstagram />
-              </v-btn>
+    <v-card flat tile class="orange d-flex justify-center" width="100%">
+      <v-card flat max-width="1100px" class="transparent d-flex flex-column flex-md-row flex-wrap justify-center my-4">
+        <v-card flat class="transparent mr-md-8 order-2 order-md-1 mt-4 mt-md-0" >
+          <div class="transparent d-flex justify-center justify-md-start">
+            <v-btn icon href="https://www.instagram.com/nailsaustralia" target="_blank" fab>
+              <IconInstagram />
+            </v-btn>
 
-              <v-btn icon href="https://www.instagram.com/nailsaustralia_shop" target="_blank" fab class="mx-4">
-                <IconInstagram />
-              </v-btn>
+            <v-btn icon href="https://www.instagram.com/nailsaustralia_shop" target="_blank" fab class="mx-4">
+              <IconInstagram />
+            </v-btn>
 
-              <v-btn icon href="https://www.facebook.com/nailsaustralia" target="_blank" fab>
-                <IconFacebook />
-              </v-btn>
-            </v-card>
+            <v-btn icon href="https://www.facebook.com/nailsaustralia" target="_blank" fab>
+              <IconFacebook />
+            </v-btn>
+          </div>
 
-            <v-card-text class="text-center text-sm-start darkGrey--text pa-0">
-              <!-- <h2>32 Brewer rd, Bentleigh 3204 VIC</h2> -->
+          <v-card-text class="text-center text-md-start darkGrey--text pa-0">
+            <!-- <h2>32 Brewer rd, Bentleigh 3204 VIC</h2> -->
 
-              <a href="mailto:nailsauinfo@gmail.com" class="darkGrey--text">
-                <h4 class="gmail">nailsauinfo@gmail.com</h4>
-              </a>
-            </v-card-text>
-          </v-col>
+            <a href="mailto:nailsauinfo@gmail.com" class="darkGrey--text">
+              <h4 class="gmail">nailsauinfo@gmail.com</h4>
+            </a>
+          </v-card-text>
+        </v-card>
 
-          <v-col v-if="categories.length && hideOnMobile" cols="12" sm="6" md="4" order="2">
-            <v-card-text class="darkGrey--text no-wrap text-center text-sm-left pa-2 pa-sm-4">
-              <h4 class="mb-2 mt-sm-2 text-center text-decoration-underline">Shop</h4>
-              <div class="d-flex justify-space-between">
-                <div v-for="(column, index) in columnsCategories" :key="index">
-                  <h4
-                    class="route mb-1"
-                    v-for="category in column"
-                    :key="category._id"
-                    @click="goToShopRoute(category.slug)"
-                  >
-                    {{ category.name }}
-                  </h4>
-                </div>
-              </div>
-            </v-card-text>
-          </v-col>
+        <v-card v-if="categories.length && $vuetify.breakpoint.mdAndUp" flat class="transparent darkGrey--text mx-8 order-md-2">
+          <h4 class="mb-2 text-center text-decoration-underline">Shop</h4>
+          <div class="d-flex justify-space-between shop-categories-column-container">
+            <div v-for="(column, index) in columnsCategories" :key="index" class="mx-4">
+              <h4
+                class="route mb-1"
+                v-for="category in column"
+                :key="category._id"
+                @click="goToShopRoute(category.slug)"
+              >
+                {{ category.name }}
+              </h4>
+            </div>
+          </div>
+        </v-card>
 
-          <v-col v-if="hideOnMobile" cols="12" sm="6" md="2" order="3">
-            <v-card-text class="darkGrey--text no-wrap text-center text-sm-left pa-2 pa-sm-4">
-              <h4 class="route mb-2 mt-sm-2 text-decoration-underline">Courses</h4>
-              <h4 class="route mb-1" @click="goToRoute('courses')">Manicure</h4>
-              <h4 class="route mb-1" @click="goToRoute('courses')">Pedicure</h4>
-              <h4 class="route mb-1" @click="goToRoute('courses')">Design</h4>
-            </v-card-text>
-          </v-col>
+        <v-card v-if="$vuetify.breakpoint.mdAndUp" flat class="transparent text-center darkGrey--text mx-8 order-md-3">
+          <!-- <v-card-text class="darkGrey--text no-wrap text-center text-sm-left pa-2 pa-sm-4"> -->
+          <h4 class="route mb-2 text-decoration-underline">Courses</h4>
+          <h4 class="route mb-1" @click="goToRoute('courses')">Manicure</h4>
+          <h4 class="route mb-1" @click="goToRoute('courses')">Pedicure</h4>
+          <h4 class="route mb-1" @click="goToRoute('courses')">Design</h4>
+          <!-- </v-card-text> -->
+        </v-card>
 
-          <v-col cols="12" sm="6" md="3" order="1" order-md="4">
-            <v-card-text class="darkGrey--text no-wrap text-center text-sm-start pa-2 pa-sm-4">
-              <h4 class="route mb-2 mt-sm-2 text-decoration-underline" @click="goToRoute('payment-and-delivery-terms')">Payment and Delivery Terms</h4>
-              <h4 class="route" @click="goToRoute('add-course')">Add course</h4>
-              <h4 class="route" @click="goToRoute('cooperation')">Cooperation</h4>
-            </v-card-text>
-          </v-col>
-        </v-row>
-      </v-container>
+        <v-card flat class="transparent text-center order-1 order-md-4 text-md-right darkGrey--text ml-md-8">
+          <h4
+            class="route mb-2 text-decoration-underline"
+            @click="goToRoute('payment-and-delivery-terms')"
+          >
+            Payment and Delivery Terms
+          </h4>
+          <h4 class="route" @click="goToRoute('add-course')">Add course</h4>
+          <h4 class="route" @click="goToRoute('cooperation')">Cooperation</h4>
+        </v-card>
+      </v-card>
     </v-card>
   </v-footer>
 </template>
@@ -84,9 +83,9 @@ export default {
   computed: {
     ...mapState(['viewportWidth']),
     ...mapState('shop', ['categories']),
-    hideOnMobile() {
-      return this.viewportWidth > 600;
-    },
+    // hideOnMobile() {
+    //   return this.viewportWidth > 600;
+    // },
     columnsCategories() {
       if (!this.categories?.length) return [];
       const len = Math.ceil(this.categories.length / 2);
@@ -111,5 +110,8 @@ export default {
 <style scoped>
 .route {
   cursor: pointer;
+}
+.shop-categories-column-container div:last-child h4 {
+  text-align: right;
 }
 </style>
